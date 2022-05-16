ce56e4739e0ccf8ed10434ac91f6e5f61e746271
Kirill <31410723+Kompanion8@users.noreply.github.com>
2022-05-16 17:43:24 +0300

    Update App.js

--- App.js
+++ App.js
@@ -5,8 +5,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppLoading from 'expo-app-loading';
 import { Camera } from 'expo-camera';
 
-// SplashScreen.preventAutoHideAsync();
-// setTimeout(SplashScreen.hideAsync, 3000);
 
 
 const fonts = () => Font.loadAsync({
@@ -42,73 +40,3 @@ export default function App() {
   
 
 };
-
-
-
-// export default class App extends Component {
-//     constructor(props) {
-//         super(props);
-//         this.state = {
-//             isImageAvailable: false,
-//             profilePic: null
-//         }
-//     }
-
-//     componentDidMount = () => {
-//         this.getImage();
-//     }
-
-//     getImage = async () => {
-//         const profilePic = await AsyncStorage.getItem("profilePic");
-//         if (profilePic) {
-//             this.setState({
-//                 isImageAvailable: true,
-//                 profilePic: JSON.parse(profilePic)
-//             });
-//         }
-//     }
-
-//     selectProfilePic = async () => {
-//         const options = {
-//             title: 'Select Avatar',
-//             storageOptions: {
-//                 skipBackup: true,
-//                 path: 'images',
-//             },
-//         };
-
-//       const result =  await ImagePicker.launchImageLibraryAsync({
-//       mediaTypes: ImagePicker.MediaTypeOptions.Images,
-//       allowsEditing: true,
-//       aspect: [4, 3],
-//       quality: 1,
-//       });
-//       console.log(result)
-
-//                 // You can also display the image using data:
-//                 // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-//                 AsyncStorage.setItem("profilePic", JSON.stringify(result.uri));
-//                 this.setState({
-//                     profilePic: result.uri,
-//                     isImageAvailable: true
-//                 });
-            
-        
-//     }
-
-//     render() {
-//         return (
-//             <View>
-              
-
-                
-                    
-//                         <TouchableOpacity onPress={this.selectProfilePic} style={{marginTop:200}}>
-//                             <Text>Choose Profile Pic</Text>
-//                         </TouchableOpacity>
-                    
-                
-//             </View>
-//         )
-//     }
-// } 
